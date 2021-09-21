0b93e249f3331d83aa6ad4f2f4fddd92e7e02ddb
mukbaniani <zura.mukbaniani.11@gmail.com>
2021-09-21 13:02:06 +0400

    added read method in controllers

--- employee/controllers/employee_controller.py
+++ employee/controllers/employee_controller.py
@@ -1,18 +1,18 @@
 from odoo import http
-from odoo.http import request
+from odoo.http import request, Response
 import json
 
 
 class EmployeeController(http.Controller):
     @http.route("/employee-list", author="public")
     def render_employee_list(self):
-        employee_list_query = request.env["employee.employee"].search([])
-        employee_list = []
-        for employee in employee_list_query:
-            values = {
-                "first_name": employee.first_name,
-                "last_name": employee.last_name,
-            }
-            employee_list.append(values)
-        data = json.dumps(employee_list)
-        return data
+        employee_list_query = (
+            request.env["employee.employee"]
+            .search([])
+            .read(["first_name", "last_name", "citizenship"])
+        )
+        return Response(
+            json.dumps(employee_list_query),
+            content_type="application/json;charset=utf-8",
+            status=200,
+        )
